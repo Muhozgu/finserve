ab780c8c62a7c08cac2b901bd7a9bdccb57e8832
Guy Muhoza <muhoza.g@outlook.com>
2026-09-16 16:26:24 +0200

    dbt initialization

--- dbt/finserve_dbt/finserve/models/BRONZE/bronze_applications.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-    materialized='view',
-    schema='BRONZE'
-) }}
-
-SELECT
-    *
-FROM {{ source('bronze', 'applications') }}
--- dbt/finserve_dbt/finserve/models/BRONZE/bronze_customers.sql
+++
@@ -1,9 +0,0 @@
-
-{{ config(
-    materialized='view',
-    schema='BRONZE'
-) }}
-
-SELECT
-    *
-FROM {{ source('bronze', 'customers') }}
